fe6fe053509bfd68ee0768dfc4d8da203d4e6255
Kaushik Jadav <kaushikjadav602@gmail.com>
2019-02-09 12:53:39 +0530

    Update index.js

--- globals/index.js
+++ globals/index.js
@@ -24,6 +24,14 @@ constants = {
         ( (date.getMonth() + 1) < 10 ? '0'+(date.getMonth() + 1) : (date.getMonth() + 1) )
          + ":" + date.getFullYear();
     },
+    getFormatedTime: (date) => {
+        return ( 
+            (date.getDate() < 10) ? '0'+date.getDate() : date.getDate() 
+        ) 
+        + ":" + 
+        ( (date.getMonth() + 1) < 10 ? '0'+(date.getMonth() + 1) : (date.getMonth() + 1) )
+         + ":" + date.getFullYear();
+    },
     MIN_RADIUS: 500
 }
 
